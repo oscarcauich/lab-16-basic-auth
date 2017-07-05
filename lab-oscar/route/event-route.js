d87348e89b1737bef7fc008ae3f24b1d43739146
Oscar Cauich <ocauich@gravitypayments.com>
2017-07-05 14:49:39 -0700

    event route middleware added

---
+++ lab-oscar/route/event-route.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const {Router} = require('express');
+
+const s3Upload = require('..lib/s3-upload-handler.js');
+const bearerAuth = require('../lib/bearer-auth-handler.js');
+const Event = require('../model/event.js');
+
+const eventRouter = module.exports = new Router();
+
+eventRouter.post('/api/events', bearerAuth, s3Upload('eventImage'), (req, res, next) => {
+  new Event({
+    title: req.body.title,
+    description: req.body.description,
+    location: req.body.location,
+    eventImageURI: req.s3Data.location,
+    userID: req.user._id.toString(),
+  })
+  .save()
+  .then(event => res.json(event))
+  .catch(next);
+});
